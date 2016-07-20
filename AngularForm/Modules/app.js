5d5858d6d5a7d52ef14e10e78eba6e63c8603ff9
Adrián Rodríguez Grillo <adrianr@pdm.es>
2016-07-20 13:08:31 +0200

    Añadido todo

---
+++ AngularForm/Modules/app.js
@@ -0,0 +1,20 @@
+﻿(function () {
+	'use strict';
+
+	var app = angular.module('app', ["ngRoute", "formulario"]);
+
+
+	/* Routeador */
+	app.config(function ($routeProvider) {
+		$routeProvider
+			.when("/", {
+				templateUrl: "form.html"
+			})
+			.when("/home", {
+				templateUrl: "form.html"
+			})
+			.otherwise({
+				redirectTo: "/"
+			});
+	});
+})();
---
+++ AngularForm/Modules/formController.js
@@ -0,0 +1,52 @@
+﻿(function () {
+	'use strict';
+
+	var formulario = angular.module('formulario', ["multiStepForm"]);
+
+	formulario.controller('formController', function ($scope, $location, $route) {
+		$scope.model = {
+			"pelicula": "-",
+			"cines": "-",
+			"sesion": "-",
+			"asientos": "-",
+			"numero": 0,
+			"precio": 0.0
+		};
+		$scope.steps = [
+			{
+				templateUrl: 'principal.html',
+				title: '0'
+			},
+			{
+				templateUrl: 'lista-peliculas.html',
+				title: '33'
+			},
+			{
+				templateUrl: 'lista-cines.html',
+				title: '33'
+			},
+			{
+				templateUrl: 'asientos.html',
+				title: '66'
+			},
+			{
+				templateUrl: 'pago.html',
+				title: '100'
+			},
+			{
+				templateUrl: 'resumen.html',
+				title: '100'
+			}
+		];
+		$scope.cancel = function () {
+			alert('Compra cancelada, será redirigido a la página principal');
+			$location.path('/home');
+			$route.reload();
+		};
+		$scope.finish = function () {
+			$location.path('/home');
+			$route.reload();
+		};
+	});
+
+})();
